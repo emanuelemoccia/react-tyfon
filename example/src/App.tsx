5ede76bc2cc3556888d1836d4945aab1d2cda968
Emanuele Moccia <emanuelemoccia@icloud.com>
2020-12-15 18:02:04 +0100

    Update App.tsx

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -3,11 +3,38 @@ import Tyfon from 'react-tyfon'
 import 'react-tyfon/dist/index.css'
 
 const App = () => {
-  Tyfon.refreshAuth().then(response => {
+  /* Tyfon.init().then(response => {
     console.log(response)
   }).catch(err => {
     console.log(err);
-  });
+  }); */
+  new Promise((resolve, reject) => {
+    Tyfon.init().then(response => {
+      resolve(response);
+    }).catch(err => {
+      reject(err);
+      console.log(err);
+    });
+  }).then(passalapalla => {
+    console.log(passalapalla);
+    return new Promise((resolve, reject) => {
+      Tyfon.checkAuth().then(response => {
+        resolve(response);
+      }).catch(err => {
+        reject(err);
+        console.log(err);
+      });
+    })
+  }).then(passa_passalapalla => {
+    console.log(passa_passalapalla);
+    Tyfon.checkAuth().then(response => {
+      console.log(response);
+    }).catch(err => {
+      console.log(err);
+    });
+  }).catch(err => {
+      console.log(err);
+  })
   
   return <div/>
 }
